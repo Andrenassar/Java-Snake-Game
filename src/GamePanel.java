f441d782536d5ee81bf20b9448dfe8c983c032c5
qaispak <qaisbadsha@gmail.com>
2016-08-02 21:55:41 -0400

    adding JPanels as 'tiles' instead of Jlabels. Need to integrate these tiles on the main game screen

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -1,33 +1,69 @@
 
 import java.awt.BorderLayout;
 import java.awt.Color;
+import java.awt.Graphics;
 import java.awt.GridLayout;
  
+
+import java.awt.Point;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 import javax.swing.BorderFactory;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.Timer;
  
-public class GamePanel extends JPanel {
+public class GamePanel extends JPanel implements ActionListener {
+		private Timer timer = new Timer(20,this);
+		private Snake snake = new Snake();
+		private GameTile[] tiles = new GameTile[625];
       
-      // private Snake snake;
-      
-       public GamePanel() {
-              this.setLayout(new GridLayout(25,25));
-              this.setBackground(Color.black);
+        public GamePanel() {
+        	this.setLayout(new GridLayout(25,25));
+            this.setBackground(Color.black);
              
-              for (int i=0; i<25; i++){
-                     for(int j=0; j<25; j++){
-                           final JLabel label = new JLabel("bla");
-                           label.setBorder(BorderFactory.createLineBorder(Color.pink));
-                           add(label);
+            	for (int i=0; i<25; i++){
+            		for(int j=0; j<25; j++){
+            			final GameTile tile = new GameTile();
+                        tile.setBorder(BorderFactory.createLineBorder(Color.pink));
+                        add(tile);
+                        //add(snake); may not need to "add" it. it will just be an indicator
                      }
-                    
-              }     
-      
-      
-      
-       }
+              }
+            	
+            	Point p = snake.snake_body.get(0);
+            	//this.getComponentAt(snake.snake_body.get(0)).setBackground(Color.cyan);;
+            	
+            	this.getComponent(50).setBackground(Color.cyan);
+            	
+            	timer.start();
+        }
+
+     
+        
+        protected void paintComponent(Graphics g){
+			super.paintComponent(g);
+			g.setColor(Color.black);
+			g.fillRect(0, 0, 800, 800);
+			
+			//snake movement (in a method later?)
+			//snake.
+				((GameTile) getComponent(50)).changeColorBlack();
+			
+		
+			
+		}
+	
+        
+		@Override
+		public void actionPerformed(ActionEvent arg0) {
+			// TODO Auto-generated method stub
+			repaint();
+			snake.repaint();
+			
+		}
       
       
       
