74d5acd0d67f11066cc2814b85f35d95260442eb
qaispak <qaisbadsha@gmail.com>
2016-08-06 15:25:59 -0400

    added constraints on snake. Boundary constraints and touching body. Game over panel

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -15,11 +15,12 @@ import java.util.ArrayList;
 import javax.swing.BorderFactory;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.Timer;
 
 public class GamePanel extends JPanel implements ActionListener, KeyListener {
-	
+
 	private int speed_factor;
 	private Timer timer;
 	private JPanel[][] tiles = new JPanel[25][25];
@@ -29,42 +30,53 @@ public class GamePanel extends JPanel implements ActionListener, KeyListener {
 	private int x_cord;
 	private int y_cord;
 	Point top_of_snake;
+	Point score_point;
+	boolean lose;
+	boolean boundary;
 	private int direction; 
 
 
 	public GamePanel() {
-		
+
 		this.setLayout(new GridLayout(25,25));
 		this.setBackground(Color.black);
-		
-		this.addKeyListener(this);
-        this.setFocusable(true);
-        this.requestFocusInWindow();
 
-        speed_factor = 150;
-        
-        timer = new Timer(speed_factor,this);
+		this.addKeyListener(this);
+		this.setFocusable(true);
+		this.requestFocusInWindow();
+
+		speed_factor = 150;
+
+		timer = new Timer(speed_factor,this);
 		timer.start();
-        
+
 		addTiles();
 		startingPoint();
 		initializeSnake();
-		
+
 		// test fruit
-		
+
 		tiles[14][22].setBackground(Color.red);
 		
-		
-		
+		lose = false; //start by not losing the game
+		boundary = false; //snake has not hit a boundary
+
+
+
 	}
 
 	protected void paintComponent(Graphics g){
 		super.paintComponent(g);
 		g.setColor(Color.black);
 		g.fillRect(0, 0, 800, 800);
-		
+
+		if(loseCheck()) {
+			System.out.println("You have lost. Game over");
 			
-		
+			JOptionPane.showMessageDialog(this, "Game Over!");
+			return;
+		}
+
 
 		//snake movement (in a method later?)
 		//left  
@@ -75,48 +87,82 @@ public class GamePanel extends JPanel implements ActionListener, KeyListener {
 		if(direction == 2 ) {
 			moveUp();
 		}
-		
+
 		if(direction == 3 ) {
 			moveDown();
 		}
-		
+
 		if(direction == 4 ) {			
 			moveRight();
 		}
-		
-		
+
+
 	}
 
+
+	private boolean loseCheck() {
+		
+		
+		if(boundary) {
+			return true;
+		}
+		
+		Point hold = snake.get(snake.size()-1);
+		
+		for(int i=0; i<snake.size()-1; i++){
+			if(snake.get(snake.size()-1).equals(snake.get(i))){
+		    	System.out.println("LOOOOOOOOOOOOOSE");
+		    	return true;
+		    }
+		}
+	
+		
+		
+		return false;
+	}
 
 	@Override
 	public void actionPerformed(ActionEvent arg0) {
 		repaint();
-				
+
 		// Speeding up the snake
 		if(System.currentTimeMillis()%1000000==0 && !(speed_factor < 30)) {
-		timer.setDelay(speed_factor-=5);
+			timer.setDelay(speed_factor-=5);
+		}
+		
+		if(loseCheck()){
+			timer.stop();
+			return;
 		}
 	}
 
 	@Override
 	public void keyPressed(KeyEvent e) {
 		// Keys for moving snake
-		
-		if(e.getKeyCode() == 37) {
-			direction  = 1;
+
+		if(e.getKeyCode() == 37) {// left
+			if (direction!=4){
+				direction  = 1;
 			}
-		
-		if(e.getKeyCode() == 38) {
-			direction  = 3;
+		}
+
+		if(e.getKeyCode() == 38) { //up
+			if(direction!=2){
+				direction  = 3;		
 			}
-		
-		if(e.getKeyCode() == 39) {
-			direction  = 4;
+		}
+
+		if(e.getKeyCode() == 39) { // right
+			if(direction!=1){
+				direction  = 4;
 			}
-		
-		if(e.getKeyCode() == 40) {
-			direction  = 2;
+		}
+
+		if(e.getKeyCode() == 40) { //down
+			if(direction!=3){
+				direction  = 2; 
 			}
+		}
 	}
 
 
@@ -124,7 +170,7 @@ public class GamePanel extends JPanel implements ActionListener, KeyListener {
 	@Override
 	public void keyReleased(KeyEvent e) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 
@@ -133,29 +179,29 @@ public class GamePanel extends JPanel implements ActionListener, KeyListener {
 		// TODO Auto-generated method stub
 	}
 
-	
+
 	public void initializeSnake(){
 		//initial snake
 		for(int i=0;i<snake_length;i++){
 			tiles[x_cord][y_cord].setBackground(Color.green);
-			
-	
+
+
 			Point snake_point  = new Point();	
 			snake_point.x = x_cord;
 			snake_point.y = y_cord;
-	
+
 			snake.add(snake_point);
-			
+
 			x_cord+=1;			
 		}
 		x_cord--;
 	}
-	
+
 	public void startingPoint(){
-			x_cord = 5;
-			y_cord = 16;	
-			direction = 4; 
-			
+		x_cord = 5;
+		y_cord = 16;	
+		direction = 4; 
+
 	}
 
 
@@ -176,22 +222,23 @@ public class GamePanel extends JPanel implements ActionListener, KeyListener {
 
 			x_cord = head_pos;				
 			tiles[x_cord][y_cord].setBackground(Color.green);
-		
+
 			Point snake_point  = new Point();
 			snake_point.x = x_cord;
 			snake_point.y = y_cord;
-		
+
 			snake.add(snake_point);
-			
-			
+
+
 			tiles[snake.get(0).x][snake.get(0).y].setBackground(Color.white);
 			snake.remove(0);
 
-			
+
 			head_pos --;
 		}
 
 		else {
+			boundary = true;
 			System.out.println("You lost");
 			return;
 		}
@@ -203,23 +250,24 @@ public class GamePanel extends JPanel implements ActionListener, KeyListener {
 			x_cord = head_pos;				
 			tiles[x_cord][y_cord].setBackground(Color.green);
 
-			
-			Point snake_point  = new Point();
-				snake_point.x = x_cord;
-				snake_point.y = y_cord;
-			
-				snake.add(snake_point);
-				
-				tiles[snake.get(0).x][snake.get(0).y].setBackground(Color.white);
-				snake.remove(0);
 
-				
-				
+			Point snake_point  = new Point();
+			snake_point.x = x_cord;
+			snake_point.y = y_cord;
+
+			snake.add(snake_point);
+
+			tiles[snake.get(0).x][snake.get(0).y].setBackground(Color.white);
+			snake.remove(0);
+
+
+
 			head_pos ++;
 		}
 
 		else {
 			System.out.println("You lost");
+			boundary = true;
 			return;
 		}
 	}
@@ -233,65 +281,67 @@ public class GamePanel extends JPanel implements ActionListener, KeyListener {
 			y_cord = head_pos;				
 			tiles[x_cord][y_cord].setBackground(Color.green);
 
-			
-			
+
+
 			Point snake_point  = new Point();
 			snake_point.x = x_cord;
 			snake_point.y = y_cord;
 
 			snake.add(snake_point);
-			
+
 			tiles[snake.get(0).x][snake.get(0).y].setBackground(Color.white);
 			snake.remove(0);
 
-			
-			
+
+
 			//tiles[x_cord][y_cord-snake_length].setBackground(Color.white);
 			head_pos ++;
 		}
 
 		else {
 			System.out.println("You lost");
+			boundary = true;
 			return;
 		}
 
-		
+
 	}
 	private void moveDown(){ // the down is actually up I think...
 		head_pos = y_cord-1;
 
 		if(head_pos >= 0  ){
 			top_of_snake = snake.get(snake.size()-1);
-			
+
 			y_cord = head_pos;				
 			tiles[x_cord][y_cord].setBackground(Color.green);
-			
+
 			Point snake_point  = new Point();
 			snake_point.x = x_cord;
 			snake_point.y = y_cord;
-		
+
 			snake.add(snake_point);
-			
+
 			if(!(tiles[14][22].equals(tiles[x_cord][y_cord]))) {
-			tiles[snake.get(0).x][snake.get(0).y].setBackground(Color.white);
-			snake.remove(0);
+				tiles[snake.get(0).x][snake.get(0).y].setBackground(Color.white);
+				snake.remove(0);
 			}
-			
+
 			else {
-				
+
 				System.out.println("We went here!");
 				snake_length++; //does it matter?
 				//do nothing else I guess
 			}
-			
+
 			head_pos --;
-		
-			
+
+
 
 		}
-		
-		
+
+
 		else {
+			boundary = true;
 			System.out.println("You lost");
 			return;
 		}
